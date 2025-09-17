5b016717b58b916b4580a6a119c13b0268d11361
Ishlok Dwivedi <ishlokdwivedi1@gmail.com>
2025-09-17 19:07:51 +0530

    Added more column for review table

--- src/main/java/org/example/uberreviewservice/models/Review.java
+++ src/main/java/org/example/uberreviewservice/models/Review.java
@@ -1,11 +1,34 @@
 package org.example.uberreviewservice.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.util.Date;
 
 @Entity
+@Table(name = "bookingReview")   // This annotation will affect on database only not affect on pojo
 public class Review {
 
     @Id // This annotation make the id property a primary key of our table
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Identity means auto_Increment
+    // by default primary key chahiye to @GeneratedValue(strategy = GenerationType.something jo tummhe chahiye lagate hain
     Long id;
+
+    @Column(nullable = false)
+    String content;
+    Double rating;
+
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)  // this annotaion tells spring about the formats of Date object to be stored i.e. Date/Time ? Timestamp
+    @CreatedDate    // This annotation tells spring only handle it for object creation
+    Date createdAt;
+
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate   // This annotation tells spring only handle it for object update
+    Date updatedAt;
+
 }
