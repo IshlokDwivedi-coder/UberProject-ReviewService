74bb33258d8408fa37bc8e99322ae19a813c022b
Ishlok Dwivedi <ishlokdwivedi1@gmail.com>
2025-09-22 15:45:51 +0530

    Some changes

---
+++ src/main/java/org/example/uberreviewservice/Repositories/driverRepository.java
@@ -0,0 +1,8 @@
+package org.example.uberreviewservice.Repositories;
+
+import org.example.uberreviewservice.models.Driver;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface driverRepository extends JpaRepository<Driver,Long> {
+
+}
--- src/main/java/org/example/uberreviewservice/Services/ReviewServices.java
+++ src/main/java/org/example/uberreviewservice/Services/ReviewServices.java
@@ -2,7 +2,9 @@ package org.example.uberreviewservice.Services;
 
 import org.apache.catalina.Store;
 import org.example.uberreviewservice.Repositories.ReviewRepository;
+import org.example.uberreviewservice.Repositories.driverRepository;
 import org.example.uberreviewservice.models.Booking;
+import org.example.uberreviewservice.models.Driver;
 import org.example.uberreviewservice.models.Review;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
@@ -14,43 +16,47 @@ import java.util.List;
 public class ReviewServices implements CommandLineRunner {
 
     private final BookingRepository bookingRepository;
+    private final driverRepository driverRepository;
 
 
     private ReviewRepository reviewRepository;
 
-    public ReviewServices(ReviewRepository reviewRepository, BookingRepository bookingRepository) {
+    public ReviewServices(ReviewRepository reviewRepository, BookingRepository bookingRepository, driverRepository driverRepository) {
         this.reviewRepository = reviewRepository;
         this.bookingRepository = bookingRepository;
+        this.driverRepository = driverRepository;
     }
 
 
     @Override
     public void run(String... args) throws Exception {
         System.out.println("**************");
+//
+//        Review r=Review.builder()
+//                .content("Amazing ride......")
+//                .rating(5.0)
+//                .build();  // create a  plain java object
+//
+//        Booking b=Booking
+//                .builder()
+//                .driverReview(r)
+//                .endTime(new Date())
+//                .build();
+//
+//        bookingRepository.save(b);
+//
+//        r.setCreatedAt( new Date() );
+//        r.setUpdatedAt( new Date() );
+//        System.out.println(r);
+//        reviewRepository.save(r);  // this code executes sql queries
+//        System.out.println(r.getId());
+//
+//        List<Review> reviews=reviewRepository.findAll();
+//
+//        for(Review review : reviews){
+//            System.out.println(review.getContent());
+//        }
 
-        Review r=Review.builder()
-                .content("Amazing ride......")
-                .rating(5.0)
-                .build();  // create a  plain java object
-
-        Booking b=Booking
-                .builder()
-                .driverReview(r)
-                .endTime(new Date())
-                .build();
-
-        bookingRepository.save(b);
-
-        r.setCreatedAt( new Date() );
-        r.setUpdatedAt( new Date() );
-        System.out.println(r);
-        reviewRepository.save(r);  // this code executes sql queries
-        System.out.println(r.getId());
-
-        List<Review> reviews=reviewRepository.findAll();
-
-        for(Review review : reviews){
-            System.out.println(review.getContent());
-        }
+        List<Driver> drivers= driverRepository.findAll(); 
     }
 }
